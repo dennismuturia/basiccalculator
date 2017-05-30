f746c535dd42419ef244fe8163491811af7a0d7f
Dennis <dmuturia2922@gmail.com>
2017-05-30 12:37:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Calculator
+
+## This is a basic Javascript calculator
+
+  Choose any button that you would choose to use
---
+++ index.js
@@ -0,0 +1,36 @@
+function addval(){
+  var num1, num2, answer;
+  num1 = parseInt(prompt("Enter number1"));
+  num2 = parseInt(prompt("Enter number2"));
+
+  answer = num1 + num2;
+
+  alert(answer);
+}
+function subval(){
+  var num1, num2, answer;
+  num1 = parseInt(prompt("Enter number1"));
+  num2 = parseInt(prompt("Enter number2"));
+
+  answer = num1 - num2;
+
+  alert(answer);
+}
+function multval(){
+  var num1, num2, answer;
+  num1 = parseInt(prompt("Enter number1"));
+  num2 = parseInt(prompt("Enter number2"));
+
+  answer = num1 * num2;
+
+  alert(answer);
+}
+function divval(){
+  var num1, num2, answer;
+  num1 = parseInt(prompt("Enter number1"));
+  num2 = parseInt(prompt("Enter number2"));
+
+  answer = num1 / num2;
+
+  alert(answer);
+}
